9f154369bfcfec75402e6c38d82bbdc934ecb390
Diego Rejalas <diegorejalas1999@hotmail.com>
2021-10-10 21:01:16 -0300

    Agregamos a github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import ClienteAxios from "./config/axios";
+
+
+//componentes 
+import Pacientes from "./components/Pacientes";
+import NuevaCita from "./components/NuevaCita";
+import Cita from "./components/Cita";
 
 function App() {
+
+  //State de la app
+  const [citas, guardarcitas] = useState([]);
+ 
+ 
+  useEffect(() => {
+    const consultarAPI = () => {
+
+      ClienteAxios.get("/pacientes")
+        .then(respuesta => {
+
+          //colocar en el state el resultado
+          guardarcitas(respuesta.data);
+          //console.log(respueta.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+
+    consultarAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route
+          exact path="/" component={() => <Pacientes citas={citas} />}
+        />
+
+        <Route
+          exact path="/nueva" component={NuevaCita}
+        />
+
+        <Route
+          exact path="/cita/:id" component={Cita}
+        />
+
+
+      </Switch>
+
+    </Router>
   );
 }
 
---
+++ src/components/Cita.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Cita = () =>{
+
+return (<h1>Desde Cita  </h1>);
+
+}
+export default Cita;
