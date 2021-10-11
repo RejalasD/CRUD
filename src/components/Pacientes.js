e263f871adede1d27f8284058bc5b76a0049452f
Diego Rejalas <diegorejalas1999@hotmail.com>
2021-10-10 23:03:03 -0300

    se agrego estilos para mostrar las horas, propietario, telefono etc

--- src/components/Pacientes.js
+++ src/components/Pacientes.js
@@ -19,10 +19,28 @@ const Pacientes = (citas) => {
 
                     <div className="col-md-8 mx-auto">
                         <div className="list-group">
-                        {citasF.map(cita => (
-                                <h3 className="mb-3">{cita.nombre}</h3>
+                            {citasF.map(cita => (
+
+                                <a key={cita._id} className="p-5 list-group-item  
+                                list-group-item-action flex-column align-items-start">
+                                    <div className="d-flex w-100 justify-content-between mb-4">
+                                        <h3 className="mb-3">{cita.nombre}</h3>
+                                        <small class="fecha-alta">
+
+                                            {cita.fecha} - {cita.hora}
+                                        </small>
+                                    </div>
+                                    <p className="mb-0">
+                                        {cita.sintomas}
+                                    </p>
+                                    <div className="contacto py-3">
+                                        <p>Dueño: {cita.propietario}</p>
+                                        <p>telefono: {cita.telefono}</p>
+                                    </div>
+                                </a>
+
                             ))}
-                            
+
                         </div>
 
                     </div>
