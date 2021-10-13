6a1067196320bd37b8aa7c64d01929c8dafbf9c6
Diego Rejalas <diegorejalas1999@hotmail.com>
2021-10-12 22:28:37 -0300

    finalizando la cita

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Fragment, Link } from 'react'
 
-const Cita = () =>{
+const Cita = ({ cita }) => {
 
-return (<h1>Desde Cita  </h1>);
+    return (
+
+
+        <Fragment>
+            <h1>Nombre cita: {cita.fecha} </h1>
+
+            <div className="container mt-5 py-5">
+                <div className="row">
+                    <div className="col-12 mb-5 d-flex justify-content-center">
+                        <Link to={"/"} className="btn btn-success text-uppercase py-2 px-5
+                        font-weight-bold">Volver</Link>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
 
 }
 export default Cita;
